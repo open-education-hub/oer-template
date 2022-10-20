3369a9c27f0e626150b1ebcfe6491363b55bbc10
Teodor Dutu <teodor.dutu@gmail.com>
2022-10-20 23:19:34 +0300

    c-makefile: Move generic C makefiles to separate folder
    Because the currently under development D generic makefiles are placed
    in `common/makefile/linux/d/`, it is fitting that C makefiles be placed
    similarly in `common/makefile/linux/c/`.
    
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- content/common/makefile/linux/c/defs.mk
+++ content/common/makefile/linux/c/defs.mk
@@ -2,7 +2,7 @@ CC = gcc
 
 # Get the relative path to the directory of the current makefile.
 MAKEFILE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-UTILS_DIR := $(MAKEFILE_DIR)../utils
+UTILS_DIR := $(MAKEFILE_DIR)../../../utils
 LOGGER_DIR := $(UTILS_DIR)/log
 
 CPPFLAGS += -I$(UTILS_DIR) -I$(LOGGER_DIR)
--- content/common/makefile/linux/c/single.mk
+++ content/common/makefile/linux/c/single.mk
@@ -2,7 +2,7 @@ all: $(BINARY)
 
 # Get the relative path to the directory of the current makefile.
 MAKEFILE_DIR = $(dir $(lastword $(MAKEFILE_LIST)))
-include $(MAKEFILE_DIR)linux.mk
+include $(MAKEFILE_DIR)c.mk
 
 $(BINARY): $(OBJS) $(LOGGER)
 	$(CC) $^ $(LDFLAGS) -o $@ $(LDLIBS)
